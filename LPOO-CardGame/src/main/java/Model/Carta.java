c62b7dba85a578b6395703da178bd84e79f816ce
vitorpf0009 <victorsantos.pf009@academico.ifsul.edu.br>
2025-05-08 17:29:45 -0300

    atl

---
+++ LPOO-CardGame/src/main/java/Model/Carta.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author vito
+ */
+public class Carta {
+
+    private int id;
+    private String nome;
+    private int ataque;
+    private int defesa;
+    private Categoria categoria;
+    private int raridade;
+
+    
+    
+}
---
+++ LPOO-CardGame/src/main/java/Model/Jogador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.List;
+
+/**
+ *
+ * @author vito
+ */
+public class Jogador {
+
+    private int id;
+    private String nickname;
+    private int level;
+    List<Carta> baralho;
+
+}
