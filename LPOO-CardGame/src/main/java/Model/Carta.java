f9af572c792e9744af784ef2f802bcb8452de4b5
vitorpf0009 <victorsantos.pf009@academico.ifsul.edu.br>
2025-05-09 15:58:54 -0300

    Update Carta.java

--- LPOO-CardGame/src/main/java/Model/Carta.java
+++ LPOO-CardGame/src/main/java/Model/Carta.java
@@ -4,17 +4,46 @@
  */
 package Model;
 
+import com.sun.istack.Nullable;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
  *
  * @author vito
  */
-public class Carta {
 
+@Entity
+@Table(name = "tb_carta")
+public class Carta implements Serializable {
+
+      @Id
+    @SequenceGenerator(name = "car_id", sequenceName = "car_id", allocationSize = 1)
+    @GeneratedValue(generator = "car_id", strategy = GenerationType.SEQUENCE)  
     private int id;
+      
+   @Column(name = "car_nome" , nullable = false)
     private String nome;
+   
+   @Column(name = "car_ataque", nullable = false)
     private int ataque;
-    private int defesa;
+
+   @Column(name = "car_defesa", nullable = false)
+   private int defesa;
+   
+      @Column(nullable = true)
+    @Enumerated(EnumType.STRING)
     private Categoria categoria;
+   
+    @Column(name = "car_raridade", nullable = true)
     private int raridade;
 
     
